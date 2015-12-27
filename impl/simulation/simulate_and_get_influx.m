57bec21ab2b4e7c272447d790467bf62d1119092
Gasper <bpaul2004@gmail.com>
2015-12-27 05:37:05 +0200

    adapterd to simple SIR

--- impl/simulation/simulate_and_get_influx.m
+++ impl/simulation/simulate_and_get_influx.m
@@ -24,7 +24,7 @@
 % simulate_and_get_influx([0.95 0.025 0 0 0.025 0 0], [0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.1], 1, 30, 360)
 
 
-function [state, I, G ] = simulate_and_get_influx(...
+function [state, I] = simulate_and_get_influx(...
     start_state, ... % start state
     params,      ... % params: see above
     delta_t,     ... % time step for simutation
@@ -33,28 +33,21 @@ function [state, I, G ] = simulate_and_get_influx(...
     )
     
     I = [];
-    G = [];
     
     current_state = start_state;
     t = 0;
     current_period = 0;
     current_i_influx = 0;
-    current_g_influx = 0;
     while(t < period)
-        [current_state, i_influx, g_influx] = ...
+        [current_state, i_influx] = ...
             simulate_single_step(current_state, params, delta_t);
         current_i_influx = current_i_influx + i_influx;
-        current_g_influx = current_g_influx + g_influx;
 
         current_period = current_period + delta_t;
         
         if (current_period >= accumulation_period)
             I = [I current_i_influx];
             current_i_influx = 0;
-            
-            G = [G current_g_influx];
-            current_g_influx = 0;
-
             current_period = 0;
         end
 
--- impl/simulation/simulate_process.m
+++ impl/simulation/simulate_process.m
@@ -1,19 +1,19 @@
 % state:
-%  (1) - S
-%  (2) - I
-%  (3) - G
+%  (1) - S            --- S // f(a,t)
+%  (2) - I            --- I // f(a,t)
+%  (3) - G           
 %  (4) - IG
-%  (5) - R_I
+%  (5) - R_I          --- R // f(a,t)
 %  (6) - R_G
 %  (7) - R_IG
 % params:
-%  (1)  - beta_I
+%  (1)  - beta_I      --- beta // f(a)
 %  (2)  - beta_G
 %  (3)  - beta_IG
-%  (4)  - gamma_I
+%  (4)  - gamma_I     --- gamma // const
 %  (5)  - gamma_G
 %  (6)  - gamma_IG
-%  (7)  - omega_I
+%  (7)  - omega_I     --- omega // f(a)
 %  (8)  - omega_G
 %  (9)  - omega_IG
 %  (10) - X
